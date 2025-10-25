79e37d7a34c172c28b570106a8bf588b44462884
davidalexander2411 <davidalexander2411@gmail.com>
2025-10-26 00:37:55 +0700

    TP Modul 7 DavidAlexander

--- Modul 5/backend/src/main/java/com/david/backend/service/PlayerService.java
+++ Modul 5/backend/src/main/java/com/david/backend/service/PlayerService.java
@@ -79,7 +79,7 @@ public class PlayerService {
     public Player updatePlayerStats(UUID playerId, Integer scoreValue, Integer coinsCollected, Integer distanceTravelled) {
         Player player = playerRepository.findById(playerId)
                 .orElseThrow(() -> new RuntimeException("Player not found with ID: " + playerId));
-        
+
         player.updateHighScore(scoreValue);
 
         // Add coins and distance to totals
